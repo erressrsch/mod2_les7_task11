5b504ce0cf2e5125b15b956f2804ac7511648176
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:21:33 +0100

    Add project files.

---
+++ mod2_les7_task11/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace mod2_les7_task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the grade [1-6]");
+
+            int grade = int.Parse(Console.ReadLine());
+            switch (grade)
+            {
+                case 1:
+                    Console.WriteLine("Unsatisfactory");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Poor");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Satisfactory");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Good");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Very good");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Excellent");
+                    break;
+            }
+        }
+    }
+}
